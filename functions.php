c5c38bcbe09f93f708e9998f3806c4a840d2164b
Rayiumir <swordart2050@gmail.com>
2026-08-24 01:44:30 +0330

    feat: Remove jQuery Migrate

--- functions.php
+++ functions.php
@@ -83,3 +83,14 @@ function Rayium_widgets(){
     );
 }
 add_action('widgets_init', 'Rayium_widgets');
+
+// Remove jQuery Migrate
+function remove_jquery_migrate( $scripts ) {
+    if ( ! is_admin() && isset( $scripts->registered['jquery'] ) ) {
+        $script = $scripts->registered['jquery'];
+        if ( $script->deps ) {
+            $script->deps = array_diff( $script->deps, array( 'jquery-migrate' ) );
+        }
+    }
+}
+add_action( 'wp_default_scripts', 'remove_jquery_migrate' );

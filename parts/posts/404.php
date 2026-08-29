1489c0167a60ef64659f9533109fee227e6db6b6
Rayiumir <swordart2050@gmail.com>
2026-08-29 04:19:10 +0330

    feat: Creating and Complation Page 404 - Archive - Category - Tag for Posts

---
+++ parts/posts/404.php
@@ -0,0 +1,9 @@
+<!-- Tag Section -->
+<section class="mt-5 mb-5">
+    <div class="text-center">
+        <span><i class="fa-duotone fa-face-sad-tear fa-10x"></i></span>
+        <h1 class="mt-4 mb-4" style="font-size: 5rem;">404</h1>
+        <p class="fs-4">متاسفانه صفحه مورد نظر شما یافت نشد.</p>
+        <a href="<?php echo home_url(); ?>" class="btn btn-primary rounded-5">بازگشت به صفحه اصلی</a>
+    </div>
+</section>
---
+++ parts/posts/archive.php
@@ -0,0 +1,34 @@
+<!-- Tag Section -->
+<section class="mt-5 mb-5">
+    <header class="mb-4 text-center">
+        <h2 class="titlec">بایگانی نوشته ها</h2>
+        <p class="text-muted">آخرین نوشته ها و نکات آموزشی ما</p>
+    </header>
+    <div class="row">
+        <?php 
+            if(have_posts()){ 
+                while(have_posts()){
+                the_post();
+        ?>
+        <div class="col-md-3 mb-4">
+            <article class="card p-3 rounded-4">
+                <figure>
+                    <?php echo the_post_thumbnail('medium', ['class' => 'img-100 rounded-4', 'loading' => 'lazy', 'alt' => esc_attr( get_the_title() ), 'title' => esc_attr( get_the_title() )]) ?>
+                </figure>
+                <h2 class="fs-2 fw-bold mt-3 mb-4"><?php the_title() ?></h2>
+                <div class="d-flex justify-content-between align-items-center">
+                    <span><?php echo getPostLikeLink( get_the_ID() ); ?></span>
+                    <span class="fs-3 text-muted"><i class="fa-duotone fa-comment"></i> <?php echo get_comments_number(); ?></span>
+                </div>
+                <a href="<?php the_permalink() ?>" class="btn btn-primary rounded-5 mt-3">مشاهده پست</a>
+            </article>
+        </div>
+        <?php 
+            }
+        }
+        ?>
+    </div>
+    <div class="mt-3">
+        <?php echo bootstrap_pagination(); ?>
+    </div>
+</section>
